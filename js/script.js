a5c98718158bcea7de5325bd3f860379d847768f
asifhasan <asifaraz@gmail.com>
2023-08-22 14:10:22 +0530

    adding all toursim files to commit

--- js/script.js
+++ js/script.js
@@ -34,6 +34,8 @@ window.onload = () => {
   } else {
     document.querySelector(".header").classList.remove("active");
   }
+  var today = new Date().toISOString().split("T")[0];
+  document.getElementsByName("setTodaysDate")[0].setAttribute("min", today);
 };
 
 var swiper = new Swiper(".home-slider", {
@@ -45,58 +47,15 @@ var swiper = new Swiper(".home-slider", {
   },
 });
 
-var swiper = new Swiper(".product-slider", {
-  loop: true,
-  grabCursor: true,
-  spaceBetween: 20,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    640: {
-      slidesPerView: 2,
-    },
-    768: {
-      slidesPerView: 3,
-    },
-    1024: {
-      slidesPerView: 4,
-    },
-  },
-});
-
-var swiper = new Swiper(".review-slider", {
-  loop: true,
-  grabCursor: true,
-  spaceBetween: 20,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    640: {
-      slidesPerView: 2,
-    },
-    768: {
-      slidesPerView: 3,
-    },
-  },
-});
-
 var swiper = new Swiper(".blogs-slider", {
   loop: true,
   autoplay: {
     delay: 2000,
+    observer: true,
+    observeParents: true,
   },
   grabCursor: true,
-  spaceBetween: 10,
+  spaceBetween: 5,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -110,26 +69,6 @@ var swiper = new Swiper(".blogs-slider", {
     },
     991: {
       slidesPerView: 3,
-    },
-  },
-});
-
-var swiper = new Swiper(".clients-slider", {
-  loop: true,
-  grabCursor: true,
-  spaceBetween: 20,
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    640: {
-      slidesPerView: 2,
-    },
-    768: {
-      slidesPerView: 3,
-    },
-    1024: {
-      slidesPerView: 4,
     },
   },
 });
